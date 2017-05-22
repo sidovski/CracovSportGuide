48d36282ce25ead2554c11bcfcff94673be55ff2
Kateryna Myrshavka <kateryna.myrshavka@gmail.com>
2017-05-22 23:40:34 +0200

    filtering

--- app/loc-list/loc-list.component.js
+++ app/loc-list/loc-list.component.js
@@ -5,17 +5,23 @@ angular.
     controller: function LocListController() {
       this.locs = [
         {
-      name: 'Swimming pool',
-      address: 'Fitnes Platinium. Gorodecka st. 16'
-    }, {
-      name: 'Swimming pool',
-      address: 'University of economic. Gorodecka st. 16'
-    }, {
-      name: 'Jogging',
-      address: 'Sport park. Zawislanska st. 20'
-    }, {
-      name: 'Jogging',
-      address: 'Sport park. Pawia st. 6'
+      name: 'Comarch Basen',
+      address: 'prof. Michała Życzkowskiego 29a',
+      tags: ["Swimming","gym","sauna"]
+    },  {
+      name: 'Fitness Platinium',
+      address: 'Pawia 5, Galeria Krakowska',
+      tags: ["cardio machines","gym","sauna","workout","functional training","fitness"]
+    },
+     {
+      name: 'Jatomi Fitness',
+      address: 'Angel City, Szlak 77',
+      tags: ["cardio machines","gym","fitness classes", "yoga","cycling","zumba"]
+    },
+     {
+      name: '72D',
+      address: 'Zakopianska 72D',
+      tags: ["cross fit","gym","trx","weightlifting","sauna", "massage","personal trainings"]
     }
       ];
     }
