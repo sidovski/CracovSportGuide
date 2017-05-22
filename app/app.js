83504d6717664f5551fe4577dc734ca9c9682503
Artem Blyzniakov <merzlak2010@yandex.ua>
2017-05-22 11:22:03 +0200

    small edits

--- app/app.js
+++ app/app.js
@@ -1,21 +1,21 @@
-// Define the `phonecatApp` module
+
 var sportguideApp = angular.module('sportguideApp', []);
 
-// Define the `PhoneListController` controller on the `phonecatApp` module
+
 sportguideApp.controller('LocListController', function LocListController($scope) {
   $scope.locs = [
     {
       name: 'Swimming pool',
-      snippet: 'Fitnes Platinium. Gorodecka st. 16'
+      address: 'Fitnes Platinium. Gorodecka st. 16'
     }, {
       name: 'Swimming pool',
-      snippet: 'University of economic. Gorodecka st. 16'
+      address: 'University of economic. Gorodecka st. 16'
     }, {
       name: 'Jogging',
-      snippet: 'Sport park. Zawislanska st. 20'
+      address: 'Sport park. Zawislanska st. 20'
     }, {
       name: 'Jogging',
-      snippet: 'Sport park. Pawia st. 6'
+      address: 'Sport park. Pawia st. 6'
     }
   ];
 });
