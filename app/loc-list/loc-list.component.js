4186d8d3af6ce8868d3fe6f1fcb4e112a9fe1ce6
Artem Blyzniakov <merzlak2010@yandex.ua>
2017-05-23 14:14:09 +0200

    add navbar, locations, base style and ng-show when write to search. create new file with locations list and delete location list from components.js

--- app/loc-list/loc-list.component.js
+++ app/loc-list/loc-list.component.js
@@ -2,27 +2,10 @@ angular.
   module('locList').
   component('locList', {
     templateUrl: 'loc-list/loc-list.template.html',    
-    controller: function LocListController() {
-      this.locs = [
-        {
-      name: 'Comarch Basen',
-      address: 'prof. Michała Życzkowskiego 29a',
-      tags: ["Swimming","gym","sauna"]
-    },  {
-      name: 'Fitness Platinium',
-      address: 'Pawia 5, Galeria Krakowska',
-      tags: ["cardio machines","gym","sauna","workout","functional training","fitness"]
-    },
-     {
-      name: 'Jatomi Fitness',
-      address: 'Angel City, Szlak 77',
-      tags: ["cardio machines","gym","fitness classes", "yoga","cycling","zumba"]
-    },
-     {
-      name: '72D',
-      address: 'Zakopianska 72D',
-      tags: ["cross fit","gym","trx","weightlifting","sauna", "massage","personal trainings"]
-    }
-      ];
+    controller: function LocListController($http) {
+      var self = this;
+      $http.get('locs/locs.json').then(function(response) {
+        self.locs = response.data;
+      });
     }
   });
