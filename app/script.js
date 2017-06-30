3a339733cb45d1ad46ddad2fe0359dd82529343e
Kateryna <kateryna.myrshavka@gmail.com>
2017-06-30 12:54:38 +0200

    last updates

---
+++ app/script.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module("sportguideApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "index.html"
+    })
+    .when("/search", {
+        templateUrl : "search.html"
+    })
+    .when("/events", {
+        templateUrl : "events.html"
+    })
+    .when("/about", {
+        templateUrl : "about.html"
+    });
+});
---
+++ script.js
@@ -0,0 +1,23 @@
+﻿var sportguideApp = angular.module('sportguideApp', ['ngRoute']);
+
+    // configure our routes
+    sportguideApp.config(function($routeProvider) {
+        $routeProvider
+
+            .when('/#', {
+                templateUrl : 'index.html',
+                controller  : 'mainController'
+            })
+
+
+            .when('/search', {
+                templateUrl : 'search.html',
+                controller  : 'searchController'
+            })
+
+            .when('/about', {
+                templateUrl : 'about.html',
+                controller  : 'aboutController'
+            });
+    });
+
